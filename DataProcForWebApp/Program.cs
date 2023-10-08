e13d8d5b559714d5b7f4eb12537d74ef83f48b71
ploskiy-2 <vvploskarev@mail.ru>
2023-10-09 00:56:45 +0300

    receiving tittle and id, using split

--- DataProcForWebApp/Program.cs
+++ DataProcForWebApp/Program.cs
@@ -5,6 +5,8 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Collections.Concurrent;
 using System.IO;
+using System.Text.RegularExpressions;
+
 
 
 namespace DataProcForWebApp
@@ -12,35 +14,45 @@ namespace DataProcForWebApp
     /// This class will contain methods for processing web application data at different stages
     public static class PipelineStages 
     {
-        ///path - the path to the directory from where to extract the file names
-        /// output - the directory where the file names will be written
-        public static Task ReadFilenamesAsync(string path,BlockingCollection<string> output)
+        ///filename - the path to the directory from where to extract the movie code
+        /// output - the dictionary where the file names will be written with them code
+        public static Task LoadContentAsync(string filename, ConcurrentDictionary<string, string> output)
         {
             return Task.Factory.StartNew(() =>
-            {
-                ///we go through all the files with the extension .tsv in all nested directories. 
-                ///Adding them to output
-                foreach (string filename in Directory.EnumerateFiles(path, "*.*", SearchOption.AllDirectories)
-                                                           .Where(s => s.EndsWith(".tsv") || s.EndsWith(".txt")))
+            {               
+                using (FileStream stream = File.OpenRead(filename))
                 {
-                    output.Add(filename);
-                }
-                ///we report that no more files will be added
-                output.CompleteAdding();
-            }, TaskCreationOptions.LongRunning); /// report that this is long running task
+                    var reader = new StreamReader(stream);
+                    string line = null;
+                    while ((line = reader.ReadLine()) != null)
+                    {
+                        ///I'll have to figure out how to optimize this
+                        string[] columns = line.Split(new char[] { '\t' }, StringSplitOptions.RemoveEmptyEntries);
+                        if ((columns[3] == "RU" || columns[3] == "EN") || (columns[4] == "RU" || columns[4] == "EN"))
+                        { output.AddOrUpdate(columns[0], columns[2], (existingKey, existingValue) => existingValue); }
+                    }
+                }           
+            }, TaskCreationOptions.LongRunning);/// report that this is long running task
         }
+
     }
     internal class Program
     {
         static async Task Main(string[] args)
         {
-            var fileNames = new BlockingCollection<string>();
-            Task readFileNames = PipelineStages.ReadFilenamesAsync(@"C:\Users\VLADIMIR\Desktop\ml-latest", fileNames);
+            ///I decided not to create a directory of the names of all the files,
+            ///since they all have different information and I need to distinguish them somehow
 
-            await readFileNames;
+            ///filename  - the path that contains movies with codes on imdb.
+            ///filmsCodeIMDB_RU_EN - dictionary containing the names and codes of 
+            ///films in Russian or English (or with such a region)
+            string filename = @"C:\Users\VLADIMIR\Desktop\ml-latest\ml-latest\MovieCodes_IMDB.tsv";
+            var filmsCodeIMDB_RU_EN = new ConcurrentDictionary<string, string>();
+            ///task which creates this dictionary
+            Task createDictionaryFilmsImdbCode = PipelineStages.LoadContentAsync(filename, filmsCodeIMDB_RU_EN);         
+            await createDictionaryFilmsImdbCode;
 
-            foreach (var file in fileNames) { Console.WriteLine(file); }
-
+            Console.WriteLine("Programm has successfully completed.");
         }
     }
 }
