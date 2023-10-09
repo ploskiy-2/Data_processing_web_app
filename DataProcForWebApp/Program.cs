9370857271f3a6cc33ab2176ee5bd5293cd6ae0a
ploskiy-2 <vvploskarev@mail.ru>
2023-10-09 08:48:28 +0300

    fix last version using regular expression

--- DataProcForWebApp/Program.cs
+++ DataProcForWebApp/Program.cs
@@ -18,6 +18,8 @@ namespace DataProcForWebApp
         /// output - the dictionary where the file names will be written with them code
         public static Task LoadContentAsync(string filename, ConcurrentDictionary<string, string> output)
         {
+            ///using regular expression to recieve tittle id and movie's tittle where region or language is RU/EN
+            string pattern = @"^(tt\d+)\s+\d+\s+(.*?)\s+(?:RU|EN)\s+\S+";
             return Task.Factory.StartNew(() =>
             {               
                 using (FileStream stream = File.OpenRead(filename))
@@ -26,10 +28,13 @@ namespace DataProcForWebApp
                     string line = null;
                     while ((line = reader.ReadLine()) != null)
                     {
-                        ///I'll have to figure out how to optimize this
-                        string[] columns = line.Split(new char[] { '\t' }, StringSplitOptions.RemoveEmptyEntries);
-                        if ((columns[3] == "RU" || columns[3] == "EN") || (columns[4] == "RU" || columns[4] == "EN"))
-                        { output.AddOrUpdate(columns[0], columns[2], (existingKey, existingValue) => existingValue); }
+                        ///recieve from line tittle and tittleid using pattern
+                        Match match = Regex.Match(line, pattern);
+                        if (match.Success)
+                        {
+                            ///update existingValue so that it stays the same
+                            output.AddOrUpdate(match.Groups[1].Value, match.Groups[2].Value, (existingKey, existingValue) => existingValue);
+                        }
                     }
                 }           
             }, TaskCreationOptions.LongRunning);/// report that this is long running task
@@ -52,6 +57,7 @@ namespace DataProcForWebApp
             Task createDictionaryFilmsImdbCode = PipelineStages.LoadContentAsync(filename, filmsCodeIMDB_RU_EN);         
             await createDictionaryFilmsImdbCode;
 
+            ///foreach (var t in filmsCodeIMDB_RU_EN) { Console.WriteLine(t.Key + " "+ t.Value); }
             Console.WriteLine("Programm has successfully completed.");
         }
     }
